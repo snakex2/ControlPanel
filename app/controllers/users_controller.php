969691b5d410d5e33785348cf1c68e731ae7f3a6
snakex2 <salazar901208@gmail.com>
2015-03-11 13:33:55 -0600

    conytol

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -5,6 +5,7 @@
 	class UsersController extends AppController
 	{
 		var $layout = 'administrador';
+		var $components = array( 'Auth');
 		function login() 
 		{
 		    //Autentificación Magica
@@ -14,5 +15,15 @@
 		{
 		    //limpio por ahora.
 		}
+
+
+	    function beforeFilter() {
+	        //Configure AuthComponent
+	        $this->Auth->authorize = 'actions';
+	        $this->Auth->loginAction = array('controller' => 'users', 'action' => 'login');
+	        $this->Auth->logoutRedirect = array('controller' => 'users', 'action' => 'login');
+	        $this->Auth->loginRedirect = array('controller' => 'posts', 'action' => 'add');
+	    }
+
 	}
  ?>
